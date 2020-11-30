1657bf013ceaf5991b7c7841bf46cb5b77c750a6
Ellen Shapiro <designatednerd@gmail.com>
2020-11-30 17:08:58 -0600

    try path again

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
     targets: [
         .target(
             name: "InflectorKit",
-            dependencies: []
+            dependencies: [],
+            path: "InflectorKit"
         )
     ]
 )
