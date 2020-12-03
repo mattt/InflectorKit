f8a31ac20b7b02e832ac953e840e9dbf8cb7eb13
Mattt <mattt@me.com>
2020-12-03 10:04:22 -0800

    Test custom singularization rule

--- Tests/InflectorKitTests/InflectorKitTests.swift
+++ Tests/InflectorKitTests/InflectorKitTests.swift
@@ -18,12 +18,14 @@ final class InflectorKitTests: XCTestCase {
         XCTAssertEqual("fish".pluralized, "fish")
     }
 
-    func testCustomPluralization() {
+    func testStringInflector() {
         let inflector = TTTStringInflector.default()
+        inflector.addSingularRule(#"^(MacBook)s (Pro|Air)?$"#, withReplacement: #"$1 $2"#)
         inflector.addPluralRule(#"^i(Pod|Pad)( Mini)?$"#, withReplacement: #"i$1s$2"#)
         inflector.addIrregular(withSingular: "lol", plural: "lolz")
         inflector.addUncountable("Herokai")
 
+        XCTAssertEqual("MacBooks Air".singularized, "MacBook Air")
         XCTAssertEqual("iPad Mini".pluralized, "iPads Mini")
         XCTAssertEqual("lol".pluralized, "lolz")
         XCTAssertEqual("Herokai".pluralized, "Herokai")
